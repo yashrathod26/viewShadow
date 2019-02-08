35679200e079c605bcfcf2e71df76ee25c9db3a8
yashrathod26 <yashthedeveloper@gmail.com>
2019-02-08 12:38:25 +0530

    added corner radius

--- viewShadow/viewShadow.swift
+++ viewShadow/viewShadow.swift
@@ -51,6 +51,29 @@ open class viewShadow:UIView {
         layer.shouldRasterize = true
         layer.rasterizationScale = scale ? UIScreen.main.scale : 1
     }
+    open func cornerRadiusAdd(radius:CGFloat,view:UIView) {
+        view.layer.cornerRadius = radius
+        view.clipsToBounds = true
+    }
+    
+    open func cornerRadiusAndShadow(baseView:UIView,parentView:UIView,radius:CGFloat,forShadow scale:Bool = true) {
+        
+        //Create view for shadow
+        let shadowView = UIView(frame: CGRect(x: baseView.frame.origin.x, y: baseView.frame.origin.y, width: baseView.frame.width, height: baseView.frame.height))
+        shadowView.layer.masksToBounds = false
+        shadowView.layer.shadowColor = UIColor.black.cgColor
+        shadowView.layer.shadowOpacity = 0.5
+        shadowView.layer.shadowOffset = CGSize(width: -1, height: 1)
+        shadowView.layer.shadowRadius = 1
+        
+        shadowView.layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+        shadowView.layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+        shadowView.backgroundColor = UIColor.clear
+        parentView.addSubview(shadowView)
+        
+        self.cornerRadiusAdd(radius: radius, view: baseView)
+    }
 }
 public enum VerticalLocation: String {
     case bottom
